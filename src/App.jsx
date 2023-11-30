2f335a0f4bea578cd2ef4b7946541290303000fd
archie <archie@localhost>
2023-11-30 03:35:20 -0600

    Fixed text overflow in <article>

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,14 @@ function App() {
 	<p>Subscribe to us on YouTube: @Example</p>
 	</div>
 	<div className="footer-child-container">
-	<p>Author:</p>
+	<p>Author and Creator:</p>
 	<p>Created by <a href="https://github.com/mr-n30">mr-n30</a></p>
-	<p>Follow me on <a href="https://github.com/mr-n30">GitHub</a>.</p>
+	<p>Source code on <a href="https://github.com/mr-n30/good-tamal-landing-page">GitHub</a></p>
 	</div>
 	<div className="footer-child-container">
 	<p>Copyright:</p>
 	<p>&copy; <a href="https://github.com/mr-n30">GoodTamal</a></p>
-	<p>&copy; <a href="https://github.com/mr-n30">mr-n30</a></p>
+	<p>&copy; Follow me on <a href="https://github.com/mr-n30">GitHub - mr-n30</a></p>
 	</div>
       </footer>
   </div>
